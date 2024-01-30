54169fc503f87a1c518be9bb92947309ebc5773b
user.email <soniccrush@naver.com>
2024-01-31 00:50:33 +0900

    UPOLOADED : project files

--- shl.py
+++ shl.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import streamlit as st 
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 
@@ -13,6 +14,40 @@ def main():
     
     # 데이터 확인
     st.write(df)
+    
+    df.loc[:,'Gender']
+    
+   
+    fig = px.scatter(df, x="Age", y="Sleep Duration")
+    labels=dict(Age="연령", Sleep_Duration="수면시간")
+    fig.update_layout(title='연령대가 수면시간에 영향을 줄까')
+    fig.update_xaxes(title_font_size =20,
+                 title_font_color='black',
+                 tickfont_family='Courier')
+    fig.update_yaxes(title_font_size =20,
+                 title_font_color='black',
+                 tickfont_family='Courier')
+    st.plotly_chart(fig)
+    
+    
+    
+    fig.update_layout(title='잠을 가장 많이 자는 직업은 무엇일까')
+    fig = px.histogram(df, x="Occupation", y="Sleep Duration",histfunc='avg')
+    fig.update_xaxes(title_font_size =20,
+                 title_font_color='black',
+                 tickfont_family='Courier')
+    fig.update_yaxes(title_font_size =20,
+                 title_font_color='black',
+                 tickfont_family='Courier')
+    st.plotly_chart(fig)
+    
+    fig = px.bar(data_frame= df, x='Age', y='Heart Rate')
+    st.plotly_chart(fig)
+    
+    fig = px.scatter(df, x='Age', y='Heart Rate', color='Stress Level', title='스트레스가 많은 사람은 심박이 높을까')
+    st.plotly_chart(fig)
+   
+    
 
 if __name__ == "__main__":
     main()
